b1cbc074a28c4e9788b5242a1f4685ed50117202
Yerlin A. Rodriguez <yerlin.alec21@gmail.com>
2025-06-18 11:53:15 -0400

    Repository changes

--- scripts/date.js
+++
@@ -1,9 +0,0 @@
-// Date JavaScript
-document.addEventListener('DOMContentLoaded', () => {
-    // Set current year for copyright
-    const currentYear = new Date().getFullYear();
-    document.getElementById('current-year').textContent = currentYear;
-    
-    // Set last modified date
-    document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
-});
